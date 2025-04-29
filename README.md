505c23411625d7201924e9a5cea6f64a56ede4d1
LeungoftheJack23 <23065363d@connect.polyu.hk>
2025-04-29 13:01:16 +0000

    2025/04/29 Added ways to generate points and load points from file

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ History:
 
 2025/04/28 Managed scripts into different directories (classes). To use it, run Rstar-tree.py as the main script.
 
+2025/04/29 Added ways to generate points and load points from file.
+
 How to use:
 
-1. Start the script using the command `python Rstar-tree.py`
-2. Enter the points, maximum children in a tree, method number and repeat times as requested in the command prompt
-3. It will show the result in the command prompt and save the result in separate files in the results directory
+1. before starting the script, you need to install the following packages using the command `pip install noise`
+2. Start the script using the command `python Rstar-tree.py`
+3. Enter the either the point file or (points, generate point method), maximum children in a tree, method number and repeat times as requested in the command prompt
+4. It will show the result in the command prompt and save the result in separate files in the results directory (and the point file if it is not from loaded file)
--- Rstar-tree.py
+++ Rstar-tree.py
@@ -3,22 +3,57 @@
 from classes.RTree import RTree
 import os
 import random
+import noise
 import time
 
 # test
 def main():    
     # ask the user for input and check if input is valid or not
     try:
-        numPoints = int(input("Enter the number of points to generate (Range: 1-10000000): "))
-        # expect int number range: [1, 10000000]
-        if numPoints <= 0:
-            print("Invalid input range: Number of points must be greater than 0.")
-            return
-        
-        maxChildren = int(input("Enter the maximum number of children per node (Range: >=2): "))
-        # maxChildren should be greater than 1 or else it is no point to create a R*-tree
-        if maxChildren <= 1:
-            print("Invalid input range: Maximum number of children must be greater than 1.")
+        loadPointFile = input("Load point file name? (Enter the file name if exists, or Enter without any input): ")
+        if loadPointFile:
+            # check if the point directory exists
+            if os.path.exists("points"):
+                if not loadPointFile.endswith(".txt"):
+                    loadPointFile += ".txt"
+                
+                try:
+                    with open(os.path.join("points", loadPointFile), "r") as f:
+                        line = f.readline()
+                        points = eval(line)
+                        numPoints = len(points)
+                        pointMethod = loadPointFile.split("p")[0][:-1]
+                        print(f"Loaded {numPoints} {pointMethod} points from {loadPointFile}.")
+                except:
+                    print(f"Error loading file: {loadPointFile}")
+                    return
+            else:
+                print(f"Directory \"points\" does not exist.")
+                return
+        else:
+            numPoints = int(input("Enter the number of points to generate (Range: 1-10000000): "))
+            # expect int number range: [1, 10000000]
+            if numPoints <= 0:
+                print("Invalid input range: Number of points must be greater than 0.")
+                return
+            
+            pointMethod = int(input("Enter the number of method to generate points [1: Random, 2: Uniform, 3: Similar, 4: Perlin Noise]: "))
+            if pointMethod == 1:
+                pointMethod = "Random"
+            elif pointMethod == 2:
+                pointMethod = "Uniform"
+            elif pointMethod == 3:
+                pointMethod = "Similar"
+            elif pointMethod == 4:
+                pointMethod = "Perlin Noise"
+            else:
+                print("Invalid input range: Method must be 1, 2, 3 or 4.")
+                return
+
+        maxChildren = int(input("Enter the maximum number of children per node (Range: >=3): "))
+        # maxChildren should be greater than 2 or else it is no point to create a R*-tree
+        if maxChildren <= 2:
+            print("Invalid input range: Maximum number of children must be greater than 2.")
             return
 
         method = int(input("Enter the number of method to implement [1: MBR Area Enlargement, 2: MBR Margin Enlargement, 3: MBR Overlap Change]: "))
@@ -32,7 +67,10 @@ def main():
             print("Invalid input range: Method must be 1, 2 or 3.")
             return
 
-        repetitions = int(input("Enter the number of times to repeat the test (Range: 1-999): "))
+        if not loadPointFile:
+            repetitions = int(input("Enter the number of times to repeat the test (Range: 1-999): "))
+        else:
+            repetitions = 1
         if repetitions <= 0:
             print("Invalid input range: Number of repetitions must be greater than 0.")
             return
@@ -45,28 +83,83 @@ def main():
 
     timeList = []
     for i in range(repetitions):
-        print(f"\n...Running test {i+1}/{repetitions}: using {method} with {numPoints} points...")
+        print(f"\n...Running test {i+1}/{repetitions}: using {method} with {numPoints} {pointMethod} points...")
         # create an R*-tree instance
         rtree = RTree(maxChildren=maxChildren, method=method)
         
-        # generate amount:numPoints unique random points (no repeats) and round positions to 2 d.p.
-        points = set()
-        while len(points) < numPoints:
-            x = round(random.uniform(0, 100), 2)
-            y = round(random.uniform(0, 100), 2)
-            points.add((x, y))
+        if not loadPointFile:
+            # generate amount = numPoints
+            # generate points method (all round positions to 2 d.p.)
+            if pointMethod == "Random":
+                # generate random points (can repeat)
+                points = set()
+                while len(points) < numPoints:
+                    x = round(random.random()*100, 2)
+                    y = round(random.random()*100, 2)
+                    points.add((x, y))
+            elif pointMethod == "Uniform":
+                # generate unique random points (no repeats)
+                points = set()
+                while len(points) < numPoints:
+                    x = round(random.uniform(0, 100), 2)
+                    y = round(random.uniform(0, 100), 2)
+                    points.add((x, y))
+            elif pointMethod == "Similar":
+                # generate base points for other points later
+                basePoints = set()
+                while len(basePoints) < pow(numPoints, 0.5):
+                    x = round(random.uniform(0, 100), 2)
+                    y = round(random.uniform(0, 100), 2)
+                    basePoints.add((x, y))
 
-        # convert set to list
-        points = list(points)
+                # apply small random offsets to base points for other points
+                points = set(basePoints)
+                while len(points) < numPoints:
+                    basePoint = random.choice(list(basePoints))
+                    offset = round(random.uniform(-0.1, 0.1), 2)
+                    simPoint = (round(basePoint[0] + offset, 2), round(basePoint[1] + offset, 2))
+                    points.add(simPoint)
+            elif pointMethod == "Perlin Noise":
+                # generate Perlin Noise points
+                points = set()
+                while len(points) < numPoints:
+                    x = round(random.uniform(0, 100), 2)
+                    y = round(random.uniform(0, 100), 2)
+                    noise_value = noise.pnoise2(x / 10, y / 10, octaves=1)
+                    if noise_value > 0.5:
+                        points.add((x, y))
+
+            # convert set to list
+            points = list(points)
+            
+            # only save the points to a file if it is not from the loaded file (Name: Random p100 001.txt, Random p100 002.txt, ...)
+            dir = "points"
+            # make directory if not exist
+            os.makedirs(dir, exist_ok=True)
+            # get all point files name
+            pointFiles = sorted([f for f in os.listdir(dir) if f.startswith(f"{pointMethod} p{numPoints}") and f.endswith(".txt")])
+            if pointFiles:
+                lastNum = int(pointFiles[-1][-7:-4])
+                nextNum = lastNum + 1
+            else:
+                nextNum = 1
+            fileName = f"{pointMethod} p{numPoints} {nextNum:03d}.txt"
+
+            # write the output to the file
+            output = []
+            output.append(str(points))
+            with open(os.path.join(dir, fileName), "w") as f:
+                f.write("\n".join(output))
+            print(f"Points file saved to \"{fileName}\"")
 
         # Measure how long it takes to construct the R*-tree
-        start_time = time.time()
+        startTime = time.time()
         for point in points:
             rtree.insert(point)
-        end_time = time.time()
+        endTime = time.time()
 
         # save time taken, later get average time taken
-        timeList.append(f"{end_time - start_time:.4f}")
+        timeList.append(f"{endTime - startTime:.4f}")
 
         # print the output first, then save to file
         print(f"Time taken to construct the R-tree: {timeList[-1]} seconds")
@@ -78,28 +171,32 @@ def main():
         output = []
         output.append(f"Method: {method}")
         output.append(f"Number of points: {numPoints}")
+        output.append(f"Generate points method: {pointMethod}")
         output.append(f"Maximum number of children: {maxChildren}")
         output.append(f"Time taken to construct the R-tree: {timeList[-1]} seconds\n")
         output.append("R*-tree structure:")
         output.append(rtree.display())
 
-        # save the test file (Name: test001.txt, test002.txt, ...)
+        # save the test file (Name: MBR Area Enlargement p100 c3 001.txt, MBR Area Enlargement p100 c3 002.txt, ...)
         dir = "results"
         # make directory if not exist
         os.makedirs(dir, exist_ok=True)
         # get all test files name
-        test_files = sorted([f for f in os.listdir(dir) if f.startswith(method + f" p{numPoints} c{maxChildren}") and f.endswith(".txt")])
-        if test_files:
-            last_test_number = int(test_files[-1][-7:-4])
-            next_test_number = last_test_number + 1
+        testFiles = sorted([f for f in os.listdir(dir) if f.startswith(method + f" p{numPoints} c{maxChildren}") and f.endswith(".txt")])
+        if testFiles:
+            lastNum = int(testFiles[-1][-7:-4])
+            nextNum = lastNum + 1
         else:
-            next_test_number = 1
-        fileName = f"{method} p{numPoints} c{maxChildren} {next_test_number:03d}.txt"
+            nextNum = 1
+        fileName = f"{method} p{numPoints} c{maxChildren} {nextNum:03d}.txt"
 
         # write the output to the file
         with open(os.path.join(dir, fileName), "w") as f:
             f.write("\n".join(output))
-        print(f"Test results saved to {fileName}")
+        print(f"Test results saved to \"{fileName}\"")
+
+    avgTime = sum([float(t) for t in timeList]) / len(timeList)
+    print(f"\nAverage time to construct the R*-tree: {avgTime:.4f} seconds")
 
 if __name__ == "__main__":
     main()
